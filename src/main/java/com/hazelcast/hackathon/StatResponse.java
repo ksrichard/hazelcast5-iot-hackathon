0b3cdccf9f99ba3a25258d8eec7e175890e41723
Sergey Dreger <sergey.dreger@hazelcast.com>
2021-08-13 12:24:16 +0300

    Add Quarkus WS controller

---
+++ src/main/java/com/hazelcast/hackathon/StatResponse.java
@@ -0,0 +1,37 @@
+package com.hazelcast.hackathon;
+
+public class StatResponse {
+
+    public StatResponse(Double streamTempCur, Double streamTempAvg, Double streamTempMin,
+        Double streamTempMax) {
+        this.streamTempCur = streamTempCur;
+        this.streamTempAvg = streamTempAvg;
+        this.streamTempMin = streamTempMin;
+        this.streamTempMax = streamTempMax;
+    }
+
+    private Double streamTempCur;
+
+    private Double streamTempAvg;
+
+    private Double streamTempMin;
+
+    private Double streamTempMax;
+
+    public Double getStreamTempCur() {
+        return streamTempCur;
+    }
+
+    public Double getStreamTempAvg() {
+        return streamTempAvg;
+    }
+
+    public Double getStreamTempMin() {
+        return streamTempMin;
+    }
+
+    public Double getStreamTempMax() {
+        return streamTempMax;
+    }
+
+}
